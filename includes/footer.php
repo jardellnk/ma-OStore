ee90490c16153e9cded9e8d1c9c99312d6b719bb
Luka Sabino <luka@gotek.com.br>
2023-07-10 20:57:00 -0300

    Fixado footer no botom

--- includes/footer.php
+++ includes/footer.php
@@ -4,7 +4,7 @@ $exec_query = mysqli_query($conn, $query);
 $row_emitente = mysqli_fetch_assoc($exec_query);
 ?>
 
-<footer class="py-3 bg-dark">
+<footer class="py-3 bg-dark fixed-bottom">
     <div class="container">
       <p class="m-0 text-center text-white">&copy; <?php echo $row_emitente['nome'] ?> - CNPJ:  <?php echo $row_emitente['cnpj'] ?></p>
       <p class="m-0 text-center text-white"> <?php echo $row_emitente['rua'] ?>, <?php echo $row_emitente['numero'] ?> - <?php echo $row_emitente['bairro'] ?>, <?php echo $row_emitente['cidade'] ?>/<?php echo $row_emitente['uf'] ?> - <?php echo $row_emitente['cep'] ?> </p>
